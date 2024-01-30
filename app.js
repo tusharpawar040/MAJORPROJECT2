6c5dff71382c8de296b13448f31c7ca8d1927293
Tushar Pawar <88307644+tusharpawar040@users.noreply.github.com>
2024-01-30 19:46:14 +0530

    app.js

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ app.use((err,req,res,next) => {
     // res.status(statusCode).send(message);
 });
 
-app.listen(8080,() => {
+app.listen(port,() => {
     console.log(`server is listening to port {port}`);
 });
 
